2d11a3b9c78ddbe622cbd785322879c4449962b0
John Paul Wile <botsone@gmail.com>
2023-08-28 20:44:00 -0600

    - set up navbar - added fieldsync logo

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react"
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+  Collapse,
+} from "@material-tailwind/react"
+import Image from "next/image"
+import fieldSyncLogo from "@/assets/fieldsync.svg"
+import Link from "next/link"
+
+export function NavbarDefault() {
+  const [openNav, setOpenNav] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    )
+  }, [])
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Home
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Save
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Fetch
+        </a>
+      </Typography>
+    </ul>
+  )
+
+  return (
+    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+      <div className="container mx-auto flex items-center justify-around text-blue-gray-900">
+        <Link href="/">
+          <Image
+            className="max-w-min h-10 mr-4 cursor-pointer py-1.5"
+            src={fieldSyncLogo}
+            alt="FieldSync Logo"
+          />
+        </Link>
+        <div className="hidden lg:block">{navList}</div>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </IconButton>
+      </div>
+      <Collapse open={openNav}>
+        <div className="container mx-auto">{navList}</div>
+      </Collapse>
+    </Navbar>
+  )
+}
